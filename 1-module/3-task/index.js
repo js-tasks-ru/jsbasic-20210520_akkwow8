4c58b827632a8d9efdfba21720f0f691712d265d
Asem Kubatkina <asemkubatkina0503@gmail.com>
2021-06-03 10:25:20 +0300

    Решение сделать первый символ заглавным

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,9 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str != '') {
+  const word = str.substr(1);
+  const firstLetter = str[0].toUpperCase();
+    return firstLetter+word;
+  } else {
+    return ('');
+  }
 }
