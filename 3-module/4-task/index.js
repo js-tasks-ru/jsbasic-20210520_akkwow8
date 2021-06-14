e1a9244e4cea516125750dda653445b178b2a93e
Asem Kubatkina <asemkubatkina0503@gmail.com>
2021-06-14 03:07:00 +0300

    Решение задачи пр функцию showSalary(users, age)

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,7 @@
 function showSalary(users, age) {
-  // ваш код...
-}
+ let minAge = users.filter(user => user.age <= age);
+ let list = minAge.map(user => `${user.name} ${user.balance}`);
+  let results = list.join('\n');
+  return results;
+};
+
