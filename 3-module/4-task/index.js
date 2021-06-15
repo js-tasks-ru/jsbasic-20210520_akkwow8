70799872015a214f874db88375108057d5348208
Asem Kubatkina <asemkubatkina0503@gmail.com>
2021-06-16 00:32:22 +0300

    Решение задачи прo min/max в строке

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,7 +1,7 @@
 function showSalary(users, age) {
  let minAge = users.filter(user => user.age <= age);
- let list = minAge.map(user => `${user.name} ${user.balance}`);
+ let list = minAge.map(user => `${user.name}, ${user.balance}`);
   let results = list.join('\n');
   return results;
 };
-
+let result = showSalary(users, age);
--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,11 @@
 function getMinMax(str) {
-  // ваш код...
+  const arr = str.split(',');
+  const arr2 = arr.join(' ');
+  const arr3 = arr2.split(' ');
+  const arr4 = arr3.filter((item) => {return parseFloat(item)});
+  let result = {
+    min: Math.min(...arr4),
+    max: Math.max(...arr4)
+}
+  return result;
 }
